8d94e0ec448c9095d66320e25ae9b515cb6ce045
Lance Cookson <lance.cookson@livingsocial.com>
2014-01-23 11:14:06 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserBase::Application.config.secret_key_base = '518b8e5a8d9e4a60877adf5b4c8aaea26f8212c456fa1e227c688b10baa659ba38b2b8c2b23cfa49e5d5c3266a7f817285bf1ad12b260c495f10edde75102843'
