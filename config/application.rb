20639c490e86e7193bb037ac8183c7ea31254506
Lance Cookson <lance.cookson@livingsocial.com>
2014-02-11 10:41:34 -0500

    Further manipulation of time.

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 
+
 module UserBase
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -16,9 +17,11 @@ module UserBase
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
   end
 end
