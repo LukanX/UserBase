e9a73645f7dc7e1b7c08aadca50037b75a1310a5
Lance Cookson <lance.cookson@livingsocial.com>
2014-02-14 16:33:43 -0500

    Improved study and interview show started methods for study index sort

--- app/models/study.rb
+++ app/models/study.rb
@@ -8,4 +8,16 @@ class Study < ActiveRecord::Base
 
   require 'date'
   validates :name, presence: true
+
+  def date_range
+    @date_range ||= []
+    self.interviews.all(order: 'scheduled_time ASC').each do |t|
+      @date_range.push(t.scheduled_time.to_date)
+    end
+    return @date_range
+  end
+
+  def first_interview
+    date_range.first
+  end
 end
