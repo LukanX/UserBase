870ad49da3ec03af70a1c1925153e0b67c3a3db0
Lance Cookson <lance.cookson@livingsocial.com>
2014-02-18 15:13:01 -0500

    Changed markup for pariticpant#show and index for better readability

--- app/models/study.rb
+++ app/models/study.rb
@@ -11,10 +11,10 @@ class Study < ActiveRecord::Base
 
   def date_range
     @date_range ||= []
-    self.interviews.all(order: 'scheduled_time ASC').each do |t|
+    self.interviews.order('scheduled_time ASC').to_a.each do |t|
       @date_range.push(t.scheduled_time.to_date)
     end
-    return @date_range
+    return @date_range.uniq
   end
 
   def first_interview
